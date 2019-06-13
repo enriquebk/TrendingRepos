db500e83e9130b4e5522dab44dc9082e7ce76377
Enrique <ebermudez.ing@gmail.com>
2019-06-12 23:37:28 -0300

    Add project's model

---
+++ TrendingRepos/Models/Owner.swift
@@ -0,0 +1,12 @@
+//
+//  Owner.swift
+//  TrendingRepos
+//
+//  Created by Enrique Bermúdez on 6/12/19.
+//  Copyright © 2019 Enrique Bermúdez. All rights reserved.
+//
+
+struct Owner {
+    let name: String
+    let avatarURL: String
+}
---
+++ TrendingRepos/Models/Repository.swift
@@ -0,0 +1,14 @@
+//
+//  Repository.swift
+//  TrendingRepos
+//
+//  Created by Enrique Bermúdez on 6/12/19.
+//  Copyright © 2019 Enrique Bermúdez. All rights reserved.
+//
+
+struct Repository {
+    let name: String
+    let description: String
+    let starsCount: String
+    let owner: Owner
+}
