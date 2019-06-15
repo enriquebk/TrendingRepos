7c797a8c0caa9e2be2652353a39afa6e95dcec10
Enrique <ebermudez.ing@gmail.com>
2019-06-15 18:43:01 -0300

    Implement get more stared repositories use case

---
+++ TrendingRepos/UseCases/GetMostStaredRepositoriesUseCase.swift
@@ -0,0 +1,28 @@
+//
+//  FetchRepositoriesUseCase.swift
+//  TrendingRepos
+//
+//  Created by Enrique Bermúdez on 6/15/19.
+//  Copyright © 2019 Enrique Bermúdez. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class GetMostStaredRepositoriesUseCase {
+    
+    private let remoteDataSource: DataSource
+    private let fromDate: Date
+    
+    init(_ remoteDataSource: DataSource = RemoteDataSource(),
+         fromDate date: Date = .thirtyDaysBeforeToday) {
+        self.remoteDataSource = remoteDataSource
+        self.fromDate = date
+    }
+    
+    func fetchPage(at pageIndex: Int) -> Observable<RepositoriesPage> {
+        return self.remoteDataSource
+            .fetch(from: fromDate,
+                   page: pageIndex)
+    }
+}
