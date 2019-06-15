8bfe824c43ebdb66af6f9882494ca2489265f088
Enrique <ebermudez.ing@gmail.com>
2019-06-15 18:44:02 -0300

    Implement repositories view model logic

--- TrendingRepos/Routes/RepositoriesRoute.swift
+++ TrendingRepos/Routes/RepositoriesRoute.swift
@@ -7,9 +7,23 @@
 //
 
 import Foundation
+import UIKit
 
 enum RepositoriesRoute: Route {
+    
+    case show(error:Error)
+    
     func getTransition() -> Transition {
-        return NavigationTransition(.noTransition)
+        
+        switch self {
+        case .show(error: let error):
+            
+            let alert = UIAlertController(title: L10n.error,
+                                          message: error.localizedDescription,
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: L10n.ok, style: .cancel))
+        
+            return NavigationTransition(.present(alert))
+        }
     }
 }
--- TrendingRepos/Scenes/Repositories/RepositoriesViewModel.swift
+++ TrendingRepos/Scenes/Repositories/RepositoriesViewModel.swift
@@ -11,24 +11,66 @@ import RxSwift
 
 class RepositoriesViewModel: ViewModel, NavigationManager {
     
-    private var currentPage: Int = 0
     var navigator: Navigator<RepositoriesRoute>!
+    private let getMostStaredRepositoriesUseCase: GetMostStaredRepositoriesUseCase
     
-    var screenName: Observable<String> {
-        return Observable.just(L10n.trendingRepos)
+    private var loadedPages: [RepositoriesPage] = [] {
+        didSet {
+            self.repositoriesSubject.onNext(self.loadedPages.flatMap({$0.repositories}))
+        }
     }
     
     private var repositoriesSubject = BehaviorSubject<[Repository]>(value: [])
+    
+    init(useCase: GetMostStaredRepositoriesUseCase = GetMostStaredRepositoriesUseCase()) {
+        self.getMostStaredRepositoriesUseCase = useCase
+    }
+    
+    var screenName: Observable<String> {
+        return Observable.just(L10n.title)
+    }
     
     var repositories: Observable<[Repository]> {
         return repositoriesSubject
     }
     
-    func loadFirstPage() {
-
+    func loadFirstPage() -> Completable {
+        return loadPage(pageIndex: 0)
     }
     
-    func loadPageForRepositories(at idexPaths: [IndexPath]) {
+    func loadPageForRepositories(at indexPaths: [IndexPath]) -> Completable {
         
+        let loadedItems = self.loadedPages.flatMap({$0.repositories}).count
+        let needsToLoadNewPage = indexPaths.first(where: {
+            return $0.item >= loadedItems - 1 
+        }) != nil
+        
+        if needsToLoadNewPage {
+            if let lastPage = self.loadedPages.last {
+                return self.loadPage(pageIndex: lastPage.pageIndex + 1)
+            }
+        }
+        return Completable.empty()
+    }
+    
+    private func loadPage(pageIndex: Int) -> Completable {
+        
+        return Completable.create { [weak self] completable -> Disposable in
+            
+            guard let strongSelf = self else { return Disposables.create() }
+            
+            return strongSelf.getMostStaredRepositoriesUseCase.fetchPage(at: pageIndex).subscribe(onNext: { page in
+                if pageIndex == 0 {
+                    self?.loadedPages = [page]
+                } else {
+                    self?.loadedPages.append(page)
+                }
+            }, onError: { error in
+                self?.navigator.navigate(to: .show(error: error))
+                completable(.error((error)))
+            }, onCompleted: {
+                completable(.completed)
+            })
+        }
     }
 }
